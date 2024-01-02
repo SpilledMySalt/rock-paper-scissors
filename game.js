d027688e88716b8d1d9bd6316a24e2f8fa3b147c
SpilledMySalt <94653739+SpilledMySalt@users.noreply.github.com>
2024-01-01 23:06:52 -0500

    Create method for functional game

--- game.js
+++ game.js
@@ -7,11 +7,34 @@ function getComputerChoice() {
 function getPlayerChoice() {
     console.log("Please select 'rock', 'paper', or 'scissors'");
     let playerSelection = prompt("Please choose one of the options: ");
+    playerSelection = playerSelection.toLowerCase(); // Convert user input to lowercase for comparison
     return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("TIE! Rematch.");
+    }
+
+    else if (playerSelection === "rock" && computerSelection === "scissors" || 
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("The player wins, ${playerSelection} beats ${computerSelection}!");
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "rock" || 
+    playerSelection === "rock" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("The computer wins, ${computerSelection} beats ${playerSelection}!");
+    }
 }
 
 // Define array of possible options for computer
 let computerSelection, playerSelection;
 computerSelection = getComputerChoice();
 playerSelection = getPlayerChoice();
+console.log("You chose ${getPlayerChoice}");
+
+// Check if player or computer won
+playRound(playerSelection, computerSelection);
 
